1b1dd9765b836f2c38fbeed3496d8e6e2860c278
yuandatou02 <yuandatou02@gmail.com>
2025-11-22 01:36:21 +0800

    更新Tauri版本并配置Vite开发环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const host = process.env.TAURI_DEV_HOST;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+   // 防止 Vite 清除 Rust 显示的错误
+  clearScreen: false,
+  server: {
+    port: 8080,
+    // Tauri 工作于固定端口，如果端口不可用则报错
+    strictPort: true,
+    // 如果设置了 host，Tauri 则会使用
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 8081,
+        }
+      : undefined,
+    watch: {
+      // 告诉 Vite 忽略监听 `src-tauri` 目录
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+  // 添加有关当前构建目标的额外前缀，使这些 CLI 设置的 Tauri 环境变量可以在客户端代码中访问
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    // Tauri 在 Windows 上使用 Chromium，在 macOS 和 Linux 上使用 WebKit
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    // 在 debug 构建中不使用 minify
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    // 在 debug 构建中生成 sourcemap
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
 })
