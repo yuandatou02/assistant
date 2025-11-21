bc0df310629aff3d901bef40aaad0f84fb965385
yuandatou02 <yuandatou02@gmail.com>
2025-11-22 01:40:33 +0800

    添加Tailwind CSS和Naive UI依赖及配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-   // 防止 Vite 清除 Rust 显示的错误
+  plugins: [vue(), tailwindcss()],
+  // 防止 Vite 清除 Rust 显示的错误
   clearScreen: false,
   server: {
     port: 8080,
@@ -16,27 +17,25 @@ export default defineConfig({
     host: host || false,
     hmr: host
       ? {
-          protocol: 'ws',
+          protocol: "ws",
           host,
           port: 8081,
         }
       : undefined,
     watch: {
       // 告诉 Vite 忽略监听 `src-tauri` 目录
-      ignored: ['**/src-tauri/**'],
+      ignored: ["**/src-tauri/**"],
     },
   },
   // 添加有关当前构建目标的额外前缀，使这些 CLI 设置的 Tauri 环境变量可以在客户端代码中访问
-  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  envPrefix: ["VITE_", "TAURI_ENV_*"],
   build: {
     // Tauri 在 Windows 上使用 Chromium，在 macOS 和 Linux 上使用 WebKit
     target:
-      process.env.TAURI_ENV_PLATFORM == 'windows'
-        ? 'chrome105'
-        : 'safari13',
+      process.env.TAURI_ENV_PLATFORM == "windows" ? "chrome105" : "safari13",
     // 在 debug 构建中不使用 minify
-    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
     // 在 debug 构建中生成 sourcemap
     sourcemap: !!process.env.TAURI_ENV_DEBUG,
   },
-})
+});
