72c95f6eecf62cd162036809da4039ad16321385
yuandatou02 <yuandatou02@gmail.com>
2025-11-23 06:22:07 +0800

    feat(queryMatch): 添加战绩查询窗口组件和窗口关闭功能

---
+++ src/queryMatch/queryMatch.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="main bg-neutral-100 dark:bg-neutral-900">
+        <div data-tauri-drag-region class="dragDiv"></div>
+        <queryHeader class="h-10 mb-2" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import queryHeader from './components/queryHeader.vue';
+</script>
