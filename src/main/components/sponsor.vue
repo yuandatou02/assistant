1d3d244bb8ec2c29dc4c988030a0b34abc25df4d
Ikaros <yuandatou02@gmail.com>
2026-03-10 13:10:48 +0800

    feat(sponsor): 添加赞助组件支持微信和支付宝支付
    - 实现了赞助对话框界面，包含卡片和警告提示
    - 集成了微信和支付宝支付方式的二维码展示
    - 添加了月度提醒功能，避免重复显示赞助提示
    - 实现了折叠面板用于切换不同支付方式
    - 添加了支付备注说明和联系方式指引

--- src/main/components/sponsor.vue
+++ src/main/components/sponsor.vue
@@ -1,5 +1,50 @@
-<template></template>
+<template>
+  <n-card :bordered="false" size="small" role="dialog" aria-modal="true">
+    <n-alert v-if="isCompleted" title="本月多次使用Frank完成对局" type="info">
+      {{ sentence }}
+      <br />[此提示每月只会弹出一次]
+    </n-alert>
 
-<script setup lang="ts"></script>
+    <n-alert v-else title="感谢您的支持❤️" type="success">
+      {{ sentence }}
+    </n-alert>
+
+    <n-collapse class="mt-4!">
+      <n-collapse-item title="微信" name="1">
+        <div class="flex flex-col items-center gap-1 justify-center">
+          <div style="width: 150px; height: 150px">
+            <img class="w-full" :src="Wechat" alt="Wechat" />
+          </div>
+          <n-tag style="font-size: 13px" checkable disabled
+            >若有任何疑问, 请在付款备注中 <br /><br />
+            留下您的问题和邮箱, 与您联系</n-tag
+          >
+        </div>
+      </n-collapse-item>
+      <n-collapse-item title="支付宝" name="2">
+        <div class="flex flex-col items-center gap-1 justify-center">
+          <div style="width: 150px; height: 150px">
+            <img class="w-full" :src="Alipay" alt="Alipay" />
+          </div>
+          <n-tag style="font-size: 13px; margin-top: 2px" checkable disabled
+            >若有任何疑问, 请在付款备注中 <br /><br />
+            留下您的问题和邮箱, 与您联系</n-tag
+          >
+        </div>
+      </n-collapse-item>
+    </n-collapse>
+  </n-card>
+</template>
+
+<script setup lang="ts">
+import { NAlert, NCard, NCollapse, NCollapseItem, NTag } from "naive-ui";
+
+const { isCompleted } = defineProps<{
+  isCompleted: boolean;
+}>();
+const sentence = "如果您觉得软件的内容有帮助，请考虑为Frank提供一点小小的赞助！您的支持，就是我追求完美代码的续航电源！！！ 😊";
+const Wechat = new URL("/src/assets/matchImage/Wechat.JPEG", import.meta.url).href;
+const Alipay = new URL("/src/assets/matchImage/Alipay.JPEG", import.meta.url).href;
+</script>
 
 <style scoped></style>
